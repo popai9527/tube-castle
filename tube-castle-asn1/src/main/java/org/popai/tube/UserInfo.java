873173d4fa6e1c438c41717147cbee863ad4b528
popai9527 <popai9527@163.com>
2025-03-29 20:35:21 +0800

    公钥联通测试

---
+++ tube-castle-asn1/src/main/java/org/popai/tube/UserInfo.java
@@ -0,0 +1,13 @@
+package org.popai.tube;
+
+/**
+ * <p>
+ * Created by popai9527 on nuc at 3/29/25 7:57 AM
+ * </p>
+ */
+
+public class UserInfo {
+    private String username;
+    private String password;
+
+}
